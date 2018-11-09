5632887f0b31c28439ab991911188f8c26d1ad3d
阮颖 <15224718167@163.com>
2018-11-09 13:31:53 +0800

    update number_guess.py

--- number_guess.py
+++ number_guess.py
@@ -5,6 +5,13 @@ Plays a game of guess the number with the user.
 import random
 
 def main():
+    """
+    Now the user will  give the lower limit and upper limit,
+    then computer will randomly create a number which is between
+    lower limit to upper limit.
+    The user need guess the number.
+    :return:
+    """
     smaller = int(input("Enter the smaller number:"))
     larger = int(input("Enter the larger number:"))
     myNumber = random.randint(smaller, larger)
